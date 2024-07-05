44e0b93384938c14f9bf97be961a4fde17055757
Gautam <0101gautakumar@gmail.com>
2024-07-06 00:52:27 +0530

    ad

--- backend/index.js
+++ backend/index.js
@@ -29,18 +29,9 @@ const corsOptions = {
   // origin: "http://localhost:5173", // Allow requests from this origin
   origin: ["https://support-sphere.vercel.app"], // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
   credentials: true,
 };
 app.use(cors(corsOptions));
-
-
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type");
-res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-// res.setHeader("Content-Type", "application/json;charset=utf-8"); // Opening this comment will cause problems
 
 // Routes
 app.get('/', (req, res) => {
