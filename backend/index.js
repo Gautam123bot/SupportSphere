44dfab44d8a4c92b4073fa2f50d17ebf9bbef849
Gautam <0101gautakumar@gmail.com>
2024-07-06 12:19:13 +0530

    final change by me

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,41 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-import servicemenRouter from "./routes/servicemen.js";
-import bookingRouter from "./routes/booking.js";
-import emailRoute from "./routes/emailRoute.js";
-import userRoute from "./routes/userRoute.js";
+import servicemenRouter from "./routes/servicemen.js"; 
+import bookingRouter from "./routes/booking.js"
+import emailRoute from "./routes/emailRoute.js"
+import userRoute from "./routes/userRoute.js"
 import cors from 'cors';
 import connectMongoDb from './connection.js';
-import complainRoute from "./routes/complainRoute.js";
+import complainRoute from "./routes/complainRoute.js"
 import ServiceMan from './models/servicemen.js';
 import Booking from './models/booking.js';
+// import twilioRouter from "./routes/servicemen.js"
 import axios from "axios";
+
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+// app.use(cors({
+  //   origin: "http://localhost:5173",
+  //   methods: ['GET', 'POST'],
+//   allowedHeaders: ['Content-Type'],
+// }));
 
 // CORS configuration
 const corsOptions = {
+  // origin: "http://localhost:5173", // Allow requests from this origin
   origin: ["https://support-sphere.vercel.app"], // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 // Routes
 app.get('/', (req, res) => {
+  console.log(req);
   return res.status(200).send('Welcome To MERN Stack Tutorial');
 });
 
@@ -35,7 +43,7 @@ app.use('/service', servicemenRouter);
 app.use("/api", userRoute);
 app.use("/booked", bookingRouter);
 app.use("/mailed", emailRoute);
-app.use("/complain", complainRoute);
+app.use("/complain", complainRoute)
 
 app.get('/bookings', async (req, res) => {
   try {
@@ -46,6 +54,7 @@ app.get('/bookings', async (req, res) => {
   }
 });
 
+
 app.get('/get-addressofservicemen/:id', async (req, res) => {
   try {
     const doc = await ServiceMan.findById(req.params.id);
@@ -53,7 +62,8 @@ app.get('/get-addressofservicemen/:id', async (req, res) => {
       return res.status(404).json({ error: 'Document not found' });
     }
     const address_staff = doc.address;
-    res.json({ address: address_staff });
+    // Send the address field as a response
+    res.json({ address: address_staff});
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -67,8 +77,10 @@ app.get('/get-addressofbooking/:id', async (req, res) => {
       return res.status(404).json({ error: 'Document not found' });
     }
     const address_booking = doc.address;
+    // Send the address field as a response
     res.json({ address: address_booking });
 
+    // Calculate distance
     const apiKey = process.env.GOOGLE_API_KEY;
     const deliveryAddress = address_booking;
     const vendorAddress = address_staff;
@@ -82,19 +94,18 @@ app.get('/get-addressofbooking/:id', async (req, res) => {
   }
 });
 
-connectMongoDb(process.env.MONGOD_URL)
-  .then(() => {
-    console.log("Mongodb connected!");
-  })
-  .catch(err => {
-    console.log("MongoDB connection error: ", err);
-    process.exit(1);
-  });
+connectMongoDb(process.env.MONGOD_URL).then(()=>{
+  console.log("Mongodb connected!")
+}).catch(err=>{
+  console.log("MongoDB connection error: ", err);
+  process.exit(1);
+})
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  // console.error(err.stack);
   res.status(500).send('Something broke!');
+  next();
 });
 
 // Start server
